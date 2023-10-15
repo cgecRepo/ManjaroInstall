e0385a64d55473d96b8090bcf2a6ee394da90abc
Carlos Espinosa <carlosespinosa2003@gmail.com>
2023-10-15 14:39:47 -0600

    Sublime Text, Shortcuts, github-cli, clang, raylib

---
+++ install.sh
@@ -0,0 +1,35 @@
+# Update database
+#sudo pacman -Syu
+
+# Keyboard shortcuts configuration
+#cp ~/Documents/scripts/ManjaroInstall/customshortcuts/kglobalshortcutsrc ~/.config/
+
+# Add openSSH to enable backup to the server
+
+# Tiling Window Manager
+
+# Github client and authentication
+# sudo pacman -S install github-cli
+# gh auth login
+
+# Julia
+
+# cpp
+#sudo pacman -S install clang
+
+# Sublime text
+#curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+#echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+#sudo pacman -Syu sublime-text
+
+# Sublime text configuration
+#subl --command "install_package_control"
+#subl --command "advanced_install_package {\"packages\": \"EasyClangComplete\"}"
+
+# Raylib
+#sudo apt install build-essential git
+#sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama
+
+# Vulkan: https://vulkan-tutorial.com/Development_environment#page_Linux
+
+# Minecraft: https://wiki.archlinux.org/title/minecraft
