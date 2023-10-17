02a79cbe0f800e7ac5ec9a1cbfdc03dbebabf7f2
Carlos Espinosa <carlosespinosa2003@gmail.com>
2023-10-17 12:53:11 -0600

    Instalacion de Raylib

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 
 # START ----------------------	YAY	--------------------------------------
 #sudo pacman -S yay
-
 # END ----------------------	YAY	--------------------------------------
 
 # Keyboard shortcuts configuration
@@ -13,19 +12,20 @@
 
 # Tiling Window Manager
 
-# START ---------------------- 	Github and client for authentication	-----------------------------------------------
+# START --------------------	Github and client for authentication	-----------------------------------------------
 #sudo pacman -S install github-cli
 #gh auth login
-# Allows file transfer
+# Allows file transfer for yay
 #git config --global protocol.file.allow always
-# END ---------------------- 	Github and client for authentication	-----------------------------------------------
+# END ----------------------	Github and client for authentication	-----------------------------------------------
 
 # Julia
+#sudo pacman -S julia
 
 # cpp
 #sudo pacman -S install clang
 
-# Sublime text
+# START --------------------------------	Sublime text 	------------------------------------
 #curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 #echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 #sudo pacman -Syu sublime-text
@@ -33,20 +33,34 @@
 # Sublime text configuration
 #subl --command "install_package_control"
 #subl --command "advanced_install_package {\"packages\": \"EasyClangComplete\"}"
+#subl --command "advanced_install_package {\"packages\": \"Julia,LSP,LSP-julia\"}"
+# END ----------------------------------	Sublime text 	------------------------------------
 
-# Raylib
-#sudo apt install build-essential git
-#sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama
+# START --------------------------------	Raylib	--------------------------------------------
+# sudo apt install build-essential git
+# sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama
+# cd 
+# cd ~/Downloads
+# git clone https://github.com/raysan5/raylib.git raylib
+# cd raylib/src/
+# make PLATFORM=PLATFORM_DESKTOP # To make the static version.
+# END ----------------------------------	Raylib	--------------------------------------------
 
-# Vulkan: https://vulkan-tutorial.com/Development_environment#page_Linux
+# START --------------------------------	Vulkan 	--------------------------------------------
+# link to tutorial: https://vulkan-tutorial.com/Development_environment#page_Linux
+# END ----------------------------------	Vulkan 	--------------------------------------------
 
-# Minecraft: https://wiki.archlinux.org/title/minecraft
+# START --------------------------------	Minecraft	----------------------------------------
+# link to downloads: https://wiki.archlinux.org/title/minecraft
 #yay -S prismlauncher
 #sudo pacman -S jdk8-openjdk
-# El mod de reindev esta en la carpeta de minecraft
+# El mod de reindev esta en la carpeta de minecraft, se necesita
+# END ----------------------------------	Minecraft	----------------------------------------
 
+# Minecraft reindev server jar
+# wget https://cdn.fox2code.com/files/reindev281-04_server.jar
 
-# START ----------------------	OPENDROP	-----------------------------------------------------
+# START --------------------------------	OPENDROP	----------------------------------------
 # permite enviar archivos atraves de airdrop
 #yay -S opendrop
-# END ----------------------	OPENDROP	-----------------------------------------------------
+# END ----------------------------------	OPENDROP	----------------------------------------
